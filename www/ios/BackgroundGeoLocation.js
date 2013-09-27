c57ae948d976cfe5ccbf08de49678c88391dbcba
Chris Scott <christocracy@gmail.com>
2013-09-27 02:47:09 -0400

    STandardize plugin arch based upon existing cordova plugins

--- www/ios/BackgroundGeoLocation.js
+++ www/ios/BackgroundGeoLocation.js
@@ -1,13 +1,34 @@
-/**
+/***
  * Custom Cordova Background GeoLocation plugin.  Uses iOS native API 
  * @author <chris@transistorsoft.com>
  * Largely based upon http://www.mindsizzlers.com/2011/07/ios-background-location/
- */
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+*/
+cordova.define("org.transistorsoft.cordova.background-geolocation", function(require, exports, module) {
+    var exec = require('cordova/exec');
 
-;(function(){
-    var BackgroundGeoLocation = function() {}
+    /**
+     * Provides access to the vibration mechanism on the device.
+     */
 
-    BackgroundGeoLocation.prototype = {
+    module.exports = {
         /**
         * Configure the native API with our authentication-token and url to POST locations to
         * TODO Native plugin assumes the json-structure as required by our rails server
@@ -44,12 +65,5 @@
             fail = (typeof(fail) === 'function') ? fail : function() {};
             return Cordova.exec(success, fail, "BackgroundGeoLocation", "stop", [options]);
         }
-    };
-
-    // remove Cordova.addConstructor since it was not supported on PhoneGap 2.0
-    if (!window.plugins) window.plugins = {};
-
-    if (!window.plugins.BackgroundGeoLocation) {
-        window.plugins.BackgroundGeoLocation = new BackgroundGeoLocation();
     }
-})();
+});
