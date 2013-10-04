697a9b57c22f3307fa4335c8ed10651e941af6de
Chris Scott <christocracy@gmail.com>
2013-10-03 22:15:37 -0400

    Harmonizing the two platforms to one common js file

---
+++ www/BackgroundGeoLocation.js
@@ -0,0 +1,52 @@
+/***
+ * Custom Cordova Background GeoLocation plugin.  Uses iOS native API 
+ * @author <chris@transistorsoft.com>
+ * @author <brian@briansamson.com>
+ * iOS native-side is largely based upon http://www.mindsizzlers.com/2011/07/ios-background-location/
+ * 
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+*/
+var exec = require("cordova/exec");
+module.exports = {
+    configure: function(success, failure, config) {
+        if (!config.auth_token || !config.url) {
+            console.log("BackgroundGeoLocation requires an auth_token and url to report to the server");
+        }
+        exec(success || function() {},
+             failure || function() {},
+             'BackgroundGeoLocation',
+             'configure',
+             [config.auth_token, config.url]);
+    },
+    start: function(success, failure, config) {
+        exec(success || function() {},
+             failure || function() {},
+             'BackgroundGeoLocation',
+             'start',
+             []);
+    },
+    stop: function(success, failure, config) {
+        exec(success || function() {},
+            failure || function() {},
+            'BackgroundGeoLocation',
+            'stop',
+            []);
+    }
+};
+
