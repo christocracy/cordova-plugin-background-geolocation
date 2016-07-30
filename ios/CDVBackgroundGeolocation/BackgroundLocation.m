f7021f6697e16b85fce5adee095fd5c3e5bef1bc
Watchara S <eakkew@gmail.com>
2016-07-30 15:04:22 +0700

    add missing attribute of location object (bearing)
    in document (https://github.com/mauron85/cordova-plugin-background-geolocation), there is bearing attribute. however there is no bearing but heading

--- ios/CDVBackgroundGeolocation/BackgroundLocation.m
+++ ios/CDVBackgroundGeolocation/BackgroundLocation.m
@@ -25,7 +25,8 @@ enum {
     instance.accuracy = [NSNumber numberWithDouble:location.horizontalAccuracy];
     instance.altitudeAccuracy = [NSNumber numberWithDouble:location.verticalAccuracy];
     instance.speed = [NSNumber numberWithDouble:location.speed];
-    instance.heading = [NSNumber numberWithDouble:location.course];
+    instance.heading = [NSNumber numberWithDouble:location.course]; // will be deprecated
+    instance.bearing = [NSNumber numberWithDouble:location.course];
     instance.altitude = [NSNumber numberWithDouble:location.altitude];
     instance.latitude = [NSNumber numberWithDouble:location.coordinate.latitude];
     instance.longitude = [NSNumber numberWithDouble:location.coordinate.longitude];
@@ -48,6 +49,7 @@ enum {
     [dict setObject:[NSNumber numberWithDouble:location.verticalAccuracy] forKey:@"altitudeAccuracy"];
     [dict setObject:[NSNumber numberWithDouble:location.speed] forKey:@"speed"];
     [dict setObject:[NSNumber numberWithDouble:location.course] forKey:@"heading"];
+    [dict setObject:[NSNumber numberWithDouble:location.course] forKey:@"bearing"];
     [dict setObject:[NSNumber numberWithDouble:location.altitude] forKey:@"altitude"];
     [dict setObject:[NSNumber numberWithDouble:location.coordinate.latitude] forKey:@"latitude"];
     [dict setObject:[NSNumber numberWithDouble:location.coordinate.longitude] forKey:@"longitude"];
@@ -74,6 +76,7 @@ enum {
     if (altitudeAccuracy != nil) [dict setObject:altitudeAccuracy forKey:@"altitudeAccuracy"];
     if (speed != nil) [dict setObject:speed forKey:@"speed"];
     if (heading != nil) [dict setObject:heading forKey:@"heading"];
+    if (bearing != nil) [dict setObject:bearing forKey:@"bearing"];
     if (altitude != nil) [dict setObject:altitude forKey:@"altitude"];
     if (latitude != nil) [dict setObject:latitude forKey:@"latitude"];
     if (longitude != nil) [dict setObject:longitude forKey:@"longitude"];
@@ -172,7 +175,7 @@ enum {
 
 - (NSString *) description
 {
-    return [NSString stringWithFormat:@"BackgroundLocation: id=%ld time=%ld lat=%@ lon=%@ accu=%@ aaccu=%@ speed=%@ head=%@ alt=%@ type=%@ debug=%d", (long)id, (long)time, latitude, longitude, accuracy, altitudeAccuracy, speed, heading, altitude, type, debug];
+    return [NSString stringWithFormat:@"BackgroundLocation: id=%ld time=%ld lat=%@ lon=%@ accu=%@ aaccu=%@ speed=%@ head=%@ alt=%@ type=%@ debug=%d", (long)id, (long)time, latitude, longitude, accuracy, altitudeAccuracy, speed, bearing, altitude, type, debug];
 }
 
 -(id) copyWithZone: (NSZone *) zone
@@ -184,6 +187,7 @@ enum {
         copy.altitudeAccuracy = altitudeAccuracy;
         copy.speed = speed;
         copy.heading = heading;
+        copy.bearing = bearing;
         copy.altitude = altitude;
         copy.latitude = latitude;
         copy.longitude = longitude;
